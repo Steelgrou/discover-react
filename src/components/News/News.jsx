0212ed869b9194cc24a3c2f2dffe6aea03653707
Steelgrou <steelgrou03@gmail.com>
2024-07-24 15:10:01 +0500

    refresh news component 24.07.2024

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -11,6 +11,7 @@ import newsFourth from '/news-swiper-fourth.webp'
 
 
 export default function News() {
+
   return (
     <section>
       <div className="container">
@@ -43,9 +44,9 @@ export default function News() {
 
           >
             <SwiperSlide>
-              <div className="news-swiper">
+              <div className="news-swiper news-swiper__first">
                 <img className='news-swiper__img' src={newsFirst} alt="" />
-
+                <div className="news-swiper__blur"></div>
                 <div className="news-swiper__before">
                   <p className='news-swiper__date'>2022-12-19</p>
                 </div>
@@ -54,62 +55,83 @@ export default function News() {
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="news-swiper">
+              <div className="news-swiper news-swiper__second">
                 <img className='news-swiper__img' src={newsSecond} alt="" />
-                <p className='news-swiper__date'>2023-01-27</p>
+                <div className="news-swiper__blur"></div>
+                <div className="news-swiper__before">
+                  <p className='news-swiper__date'>2023-01-27</p>
+                </div>
                 <h3 className='news-swiper__text'>ДЕНЬ ПРИНЯТИЯ КОНСТИТУЦИИ РЕСПУБЛИКИ УЗБЕКИСТАН</h3>
                 <h5 className='news-swiper__link'>Подробнее</h5>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="news-swiper">
+              <div className="news-swiper news-swiper__third">
                 <img className='news-swiper__img' src={newsThird} alt="" />
-                <p className='news-swiper__date'>2022-12-31</p>
+                <div className="news-swiper__blur"></div>
+                <div className="news-swiper__before">
+                  <p className='news-swiper__date'>2022-12-31</p>
+                </div>
                 <h3 className='news-swiper__text'>Мы продолжаем знакомить Вас с многофункциональными объектами Discover Invest.</h3>
                 <h5 className='news-swiper__link'>Подробнее</h5>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="news-swiper">
+              <div className="news-swiper news-swiper__fourth">
                 <img className='news-swiper__img' src={newsFourth} alt="" />
-                <p className='news-swiper__date'>2022-10-02</p>
+                <div className="news-swiper__blur"></div>
+                <div className="news-swiper__before">
+                  <p className='news-swiper__date'>2022-10-02</p>
+                </div>
                 <h3 className='news-swiper__text'>Кто такой менеджер строительного проекта?</h3>
                 <h5 className='news-swiper__link'>Подробнее</h5>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="news-swiper">
+              <div className="news-swiper news-swiper__fifth">
                 <img className='news-swiper__img' src={newsFirst} alt="" />
-                <p className='news-swiper__date'>2022-12-19</p>
+                <div className="news-swiper__blur"></div>
+                <div className="news-swiper__before">
+                  <p className='news-swiper__date'>2022-12-19</p>
+                </div>
                 <h3 className='news-swiper__text'>Компания Discover Invest поздравляет всех с наступающим 2023 годом!</h3>
                 <h5 className='news-swiper__link'>Подробнее</h5>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="news-swiper">
+              <div className="news-swiper news-swiper__sixth">
                 <img className='news-swiper__img' src={newsSecond} alt="" />
-                <p className='news-swiper__date'>2023-01-27</p>
+                <div className="news-swiper__blur"></div>
+                <div className="news-swiper__before">
+                  <p className='news-swiper__date'>2023-01-27</p>
+                </div>
                 <h3 className='news-swiper__text'>ДЕНЬ ПРИНЯТИЯ КОНСТИТУЦИИ РЕСПУБЛИКИ УЗБЕКИСТАН</h3>
                 <h5 className='news-swiper__link'>Подробнее</h5>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="news-swiper">
+              <div className="news-swiper news-swiper__seventh">
                 <img className='news-swiper__img' src={newsThird} alt="" />
-                <p className='news-swiper__date'>2022-12-31</p>
+                <div className="news-swiper__blur"></div>
+                <div className="news-swiper__before">
+                  <p className='news-swiper__date'>2022-12-31</p>
+                </div>
                 <h3 className='news-swiper__text'>Мы продолжаем знакомить Вас с многофункциональными объектами Discover Invest.</h3>
                 <h5 className='news-swiper__link'>Подробнее</h5>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="news-swiper">
+              <div className="news-swiper news-swiper__eighth">
                 <img className='news-swiper__img' src={newsFourth} alt="" />
-                <p className='news-swiper__date'>2022-10-02</p>
+                <div className="news-swiper__blur"></div>
+                <div className="news-swiper__before">
+                  <p className='news-swiper__date'>2022-10-02</p>
+                </div>
                 <h3 className='news-swiper__text'>Кто такой менеджер строительного проекта?</h3>
                 <h5 className='news-swiper__link'>Подробнее</h5>
+                
               </div>
             </SwiperSlide>
-
           </Swiper>
         </div>
       </div>
